18fb3c5b06b0c8bae4e246e09ebbf9fb23efc820
Monica Butarbutar <monicabutarbutar04@gmail.com>
2023-05-26 15:40:40 +0700

    Initial Commit

---
+++ GIT-Exercise-Blessed/GIT_Exercise_BlessedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_Exercise_BlessedApp.swift
+//  GIT-Exercise-Blessed
+//
+//  Created by Monica Butarbutar on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GIT_Exercise_BlessedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
